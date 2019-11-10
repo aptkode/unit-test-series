b3282aa2def91b958f7407504123580a0b5aaf24
Ruwanka <ruwankamadhushan@gmail.com>
2019-11-10 08:40:50 +0530

    end of third episode

--- src/test/java/com/aptkode/math/MathHelperTest.java
+++ src/test/java/com/aptkode/math/MathHelperTest.java
@@ -1,18 +1,31 @@
 package com.aptkode.math;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MathHelperTest {
 
-    @Test
-    void factorialTest() {
-        assertEquals(1, MathHelper.factorial(0));
-        assertEquals(1, MathHelper.factorial(1));
-        assertEquals(120, MathHelper.factorial(5));
-        assertEquals(720, MathHelper.factorial(6));
+    @ParameterizedTest(name = "[{index}] - {0} should return {1}")
+    @MethodSource("factorialTestSource")
+    void factorialTest( int number, int expectedValue ) {
+        assertEquals(expectedValue, MathHelper.factorial(number));
+    }
+
+    private static Stream<Arguments> factorialTestSource()
+    {
+        return Stream.of(
+                Arguments.of( 0, 1 ),
+                Arguments.of( 1, 1 ),
+                Arguments.of( 5, 120 ),
+                Arguments.of( 6, 720 )
+        );
     }
 
     @Test
